f8eabdd58b86a25917301cd6d045e5863e5c3102
Quynh To Tuan <quynh.totuan@gmail.com>
2020-05-20 11:56:34 +0200

    feat(bootcamps): add fields selector

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,11 +9,27 @@ const asyncHandler = require('../middleware/async')
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
     let query
 
-    let queryStr = JSON.stringify(req.query)
+    const reqQuery = { ...req.query }
 
+    // Fields to exclude
+    const removeFields = ['select']
+
+    // Loop over remove fields and delete them from reqQuery
+    removeFields.forEach(param => delete reqQuery[param])
+
+    let queryStr = JSON.stringify(reqQuery)
+
+    // Create operators ($gt, $gte, etc)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
+    // Finding resource
     query = Bootcamp.find(JSON.parse(queryStr))
+
+    // Select fields
+    if (req.query.select) {
+        const fields = req.query.select.split(',').join(' ')
+        query = query.select(fields)
+    }
 
     const bootcamps = await query
 
