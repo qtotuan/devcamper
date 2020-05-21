e8076df5e90698aa72b090fecf22d2aaa5848c77
Quynh To Tuan <quynh.totuan@gmail.com>
2020-05-21 11:09:13 +0200

    feat(middleware): handle pagination, filters, selects, operators

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,6 +8,8 @@ const {
     getBootcampsInRadius,
     bootcampPhotoUpload
 } = require('../controllers/bootcamps')
+const advancedResults = require('../middleware/advancedResults')
+const Bootcamp = require('../models/Bootcamp')
 
 // Include other resource routers
 const courseRouter = require('./courses')
@@ -22,7 +24,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 router.route('/:id/photo').put(bootcampPhotoUpload)
 
 router.route('/')
-    .get(getBootcamps)
+    .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
     .post(createBootcamp)
 
 router.route('/:id')
