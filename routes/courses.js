bb8f51186b2d9c1f666f58c7a9ef56e7e6a9ebd1
Quynh To Tuan <quynh.totuan@gmail.com>
2020-05-23 08:38:00 +0200

    feat(auth): protect routes for specific user roles

--- routes/courses.js
+++ routes/courses.js
@@ -6,7 +6,7 @@ const {
   updateCourse,
   deleteCourse
 } = require('../controllers/courses')
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 const Course = require('../models/Course')
 const advancedResults = require('../middleware/advancedResults')
@@ -19,12 +19,12 @@ router
     path: 'bootcamp',
     select: 'name description'
   }), getCourses)
-  .post(protect, addCourse)
+  .post(protect, authorize('publisher', 'admin'), addCourse)
 
 router
   .route('/:id')
   .get(getCourse)
-  .put(protect, updateCourse)
-  .delete(protect, deleteCourse)
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse)
   
 module.exports = router
