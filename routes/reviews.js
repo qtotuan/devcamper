ca983b45144ebf1d4a81b273f19cc792ccb06b4e
Quynh To Tuan <quynh.totuan@gmail.com>
2020-05-24 09:53:34 +0200

    feat(reviews): add update and delete routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,9 @@ const express = require('express')
 const {
   getReviews,
   getReview,
-  addReview
+  addReview,
+  updateReview,
+  deleteReview
 } = require('../controllers/reviews')
 const advancedResults = require('../middleware/advancedResults')
 const Review = require('../models/Review')
@@ -21,5 +23,7 @@ router
 router
   .route('/:id')
   .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview)
 
 module.exports = router
